2982e6dbeff5e3e1c5bfab3bf86138d984b402ff
Diego C <dcanchecmd@gmail.com>
2026-09-10 23:18:13 -0600

    feat(login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,25 +10,67 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
 
+  // Control para mostrar/ocultar contraseña
+  bool _obscure = true;
   @override
   Widget build(BuildContext context) {
-      //Para obtener el tamaño de la pantalla
+    //Para obtener el tamaño de la pantalla
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding:EdgeInsets.symmetric(horizontal: 20) ,
+          padding: EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
               SizedBox(
                 width: size.width,
                 height: 200,
-                child: RiveAnimation.asset('login-bear.riv'),
-              )
+                child: IgnorePointer(
+                  child: RiveAnimation.asset('assets/login-bear.riv'),
+                )
+              ),
+              //Para separar espacio
+              SizedBox(height: 10),
+              // Campo de texto para Email
+              TextField(
+                // Para mostrar el teclado
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  prefixIcon: const Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    )
+                ),
+              ),
+              // Campo de texto para contraseña
+              SizedBox(height: 10),
+              TextField(
+                obscureText: _obscure,
+                // Para mostrar el teclado
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscure ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      //Refrescar el ícono
+                      setState(() {
+                        _obscure = !_obscure;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    )
+                ),
+              ),
             ],
           ),
-        ) 
-      )
+          ),
+      ),
     );
   }
 }
